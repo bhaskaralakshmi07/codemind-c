2b70b62c29f62aabc02665d0010ba6be06bf6303
bhaskaralakshmi07 <97425746+bhaskaralakshmi07@users.noreply.github.com>
2022-04-23 15:14:28 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int r,s,sum=0;     s=n;     while(n!=0)     {         r=n%10;         sum=sum+r;         n=n/10;     }     if(s%sum==0)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Harshed_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int r,s,sum=0;
+    s=n;
+    while(n!=0)
+    {
+        r=n%10;
+        sum=sum+r;
+        n=n/10;
+    }
+    if(s%sum==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
