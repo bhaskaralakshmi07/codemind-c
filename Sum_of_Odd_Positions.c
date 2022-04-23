e470452ceb2a2dbba3664837e4600e9c751f8802
bhaskaralakshmi07 <97425746+bhaskaralakshmi07@users.noreply.github.com>
2022-04-23 15:36:14 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n],os=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(i%2!=0)         os=os+a[i];     }     printf("%d",os);     return 0; }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int a[n],os=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2!=0)
+        os=os+a[i];
+    }
+    printf("%d",os);
+    return 0;
+}
